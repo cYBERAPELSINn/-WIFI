54130267e23d8cd2dacc03381343700c29316aba
cYBERAPELSINn <ilssaf2010@gmail.com>
2025-03-12 20:13:04 +0300

    Add files via upload

---
+++ MACADRESS.ino
@@ -0,0 +1,12 @@
+#include <ESP8266WiFi.h>
+
+void setup() {
+    Serial.begin(115200);
+    delay(1000);
+    Serial.print("MAC address: ");
+    Serial.println(WiFi.macAddress());
+}
+
+void loop() {
+    // Do nothing
+}
